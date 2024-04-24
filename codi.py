56db8443324d0d75d71d8c2eddec75280b1ba507
Alina-Sapunova <alina090207@mail.ru>
2024-04-24 23:55:36 +0300

    бот старт и хелп

---
+++ codi.py
@@ -0,0 +1,2 @@
+TOKEN = '7197643700:AAGDeHtbql7ZykEzxCIEmLaoBDZAGwdlE-I'
+# 5796309424  чат id
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+# 5796309424  чат id
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import CommandHandler
+from telegram.ext import ApplicationBuilder
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TOKEN = '7197643700:AAGDeHtbql7ZykEzxCIEmLaoBDZAGwdlE-I'
+
+proxy_url = "socks5://user:pass@host:port"
+
+app = ApplicationBuilder().token(TOKEN).proxy_url(proxy_url).build()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def start(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+    )
 
 
-# Press the green button in the gutter to run the script.
+async def help_command(update, context):
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+def main():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

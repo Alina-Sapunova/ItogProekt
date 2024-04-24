c844945b3ce92b8541236ee98776accda762d512
Alina-Sapunova <alina090207@mail.ru>
2024-04-25 01:00:56 +0300

    создание клавиатуры

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler
 from telegram.ext import ApplicationBuilder
+import sqlite3
+from telegram import ReplyKeyboardMarkup
 
 TOKEN = '7197643700:AAGDeHtbql7ZykEzxCIEmLaoBDZAGwdlE-I'
 
@@ -18,24 +20,36 @@ logger = logging.getLogger(__name__)
 
 
 async def start(update, context):
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
-    )
+    reply_keyboard = [['/Да', '/Нет'],
+                      ['/site', '/work_time']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    await update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+            reply_markup=markup
+        )
+    # user = update.effective_user
+    # await update.message.reply_html(
+    #     rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+    # )
 
 
 async def help_command(update, context):
     await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
 
 
+async def photo_command(update, context):
+    conn = sqlite3.connnect('bot.db')
+    cur = conn.cursor()
+    result = cur.execute("SELECT photo FROM party WHERE id = ?", (1,)).fetchone()[0]
+
+
 def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    # Запускаем приложение.
+    application.add_handler(CommandHandler("photo", photo_command))
     application.run_polling()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
